76554e1550c5e4a9574726ed92d3366001686450
andretmarques <andre.t.marques@tecnico.ulisboa.pt>
2022-01-17 02:26:24 +0000

    moved image to constants

--- lib/landingpage/LandingPage.dart
+++ lib/landingpage/LandingPage.dart
@@ -1,6 +1,7 @@
+import 'package:unihub/constants/Constants.dart' as Constants;
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:unihub/constants/ImageConstants.dart' as ImageConstants;
+import 'package:unihub/utils/HalfLogoUtils.dart';
+import 'package:unihub/utils/LandingPageUtils.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key , required this.username}) : super(key: key);
@@ -15,55 +16,21 @@ class LandingPage extends StatefulWidget {
 class _LandingWelcomePageState extends State<LandingPage> {
   _LandingWelcomePageState ({Key? key , required this.username});
   final String username;
+
+  var halfLogoUtils = HalfLogoUtils();
+  var landingPageUtils = LandingPageUtils();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          Scaffold(
-              body: Center(
-                child: Image.asset(ImageConstants.HALF_LOGO, scale: 1.5),
-                heightFactor: 1.7,
-      )
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color.fromRGBO(88, 136, 204, 1),
-              ),
-              width: MediaQuery.of(context).size.width - 50,
-            )
-          ),
-          Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text( "Welcome!",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.roboto(
-                          fontSize: 36,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                      )
-
-                  ),
-                  const SizedBox(height: 15),
-                  Text( "User " + username,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.roboto(
-                        fontSize: 24,
-                        color: Colors.white
-                    ),
-                  ),
-                ],
-              ),
-          ),
+          halfLogoUtils.loadHalfLogo(Constants.HALF_LOGO_POSITIONS.LEFT),
+          landingPageUtils.createWelcomeCircle(context),
+          landingPageUtils.createWelcomeString(username),
         ],
-        ),
+      ),
     );
   }
 }
--- lib/main/UniHubApp.dart
+++ lib/main/UniHubApp.dart
@@ -23,7 +23,7 @@ class UniHubApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: const Color.fromRGBO(88, 136, 204, 1),
       ),
       initialRoute: '/LandingPage',
       routes: {
