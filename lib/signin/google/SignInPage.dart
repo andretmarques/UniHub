4939c192fb681202d414aa90489cd151c6ee7f50
joaoachando <55790524+joaoachando@users.noreply.github.com>
2022-01-18 01:58:46 +0000

    Update SignInPage.dart

--- lib/signin/google/SignInPage.dart
+++ lib/signin/google/SignInPage.dart
@@ -4,13 +4,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInProvider extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
-  GoogleSignInAccount? _user;
-  GoogleSignInAccount get user => _user!;
 
   Future googleLogin() async {
     final googleUser = await googleSignIn.signIn();
-    if(googleUser == null) return;
-    _user = googleUser;
+    if (googleUser == null) return;
     final googleAuth = await googleUser.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
