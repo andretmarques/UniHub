8dc989dc84a27f5e48964bcf72b583516f1b6184
joaoachando <55790524+joaoachando@users.noreply.github.com>
2022-01-18 02:26:31 +0000

    Firebase (#5)
    * Google login init
    
    * Silent login
    
    * Update SignInPage.dart
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update UniHubApp.dart
    
    * Final commit firebase
    
    * Change icon size

---
+++ lib/login/SilentLogin.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:unihub/landingpage/LandingPage.dart';
+import 'package:unihub/login/LoginPage.dart';
+
+class SilentLogin extends StatelessWidget {
+  const SilentLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(),);
+          } else if (snapshot.hasData) {
+            String user = (snapshot.data as User).displayName ?? "Error";
+            return LandingPage(username: user);
+          } else if (snapshot.hasError) {
+            return const Center(child: Text("Something Went wrong!"),);
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/main/UniHubApp.dart
+++ lib/main/UniHubApp.dart
@@ -1,37 +1,68 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:unihub/login/LoginPage.dart';
 import 'package:unihub/registerpage/RegisterPage.dart';
 import 'package:unihub/taskpages/homepage/HomePage.dart';
 import 'package:unihub/landingpage/LandingPage.dart';
+import 'package:unihub/login/SilentLogin.dart';
+import 'package:unihub/signin/google/SignInPage.dart';
+import 'package:page_transition/page_transition.dart';
 
 class UniHubApp extends StatelessWidget {
-  const UniHubApp({Key? key}) : super(key: key);
+
 
   // TODO get username
   static const username = "username";
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'UniHub',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-      ),
-      initialRoute: '/LandingPage',
-      routes: {
-        '/LandingPage': (context) => const LandingPage(username: username),
-        '/LoginPage': (context) => const LoginPage(),
-        '/RegisterPage': (context) => const RegisterPage()
-      },
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child:MaterialApp(
+        title: 'UniHub',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+          future: firebase,
+          builder: (context, snapshot) {
+            if(snapshot.hasError) {
+              print('You Have an error! ${snapshot.error.toString()}');
+              return const Text('Something Went Wrong!');
+            } else if (snapshot.hasData) {
+              return AnimatedSplashScreen(splash: 'assets/images/half_logo.png',
+                splashIconSize: MediaQuery.of(context).size.width - 120,
+                pageTransitionType: PageTransitionType.bottomToTop,
+                duration: 1000, nextScreen: const SilentLogin(),
+              );
+            }
+            else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+        // initialRoute: '/LoginPage',
+        routes: {
+           '/LandingPage': (context) => const LandingPage(username: username),
+           '/LoginPage': (context) => const LoginPage()
+        },
+
+      )
     );
   }
+
 }
