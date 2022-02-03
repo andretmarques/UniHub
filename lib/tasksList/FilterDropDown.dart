b3ba02c142f4db7637e16cbc3be487a2b125d18b
miguelrnf <47886922+miguelrnf@users.noreply.github.com>
2022-02-03 16:02:14 +0000

    Feature/drop down (#26)
    * filter dropDown
    
    * filter dropDown
    
    * drop down filter task
    
    * small fix ui cenas

---
+++ lib/tasksList/FilterDropDown.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:unihub/tasksList/arrow_clipper.dart';
+
+import 'TaskList.dart';
+
+class FilterDropDown extends StatefulWidget {
+  final List icons;
+  final Color backgroundColor;
+  final ValueChanged<int> onChange;
+
+  const FilterDropDown({
+    Key? key,
+    required this.icons,
+    this.backgroundColor = Colors.white,
+    required this.onChange,
+  })  : super(key: key);
+  @override
+  _FilterDropDownState createState() => _FilterDropDownState();
+}
+
+class _FilterDropDownState extends State<FilterDropDown>
+    with SingleTickerProviderStateMixin {
+  late GlobalKey _key;
+  bool isMenuOpen = false;
+  late Offset buttonPosition;
+  late Size buttonSize;
+  late OverlayEntry _overlayEntry;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+    _key = LabeledGlobalKey("button_icon");
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    closeMenu();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  findButton() {
+    RenderObject? renderBox = _key.currentContext?.findRenderObject();
+    if (renderBox != null) {
+      buttonSize = (renderBox as RenderBox).size;
+      buttonPosition = renderBox.localToGlobal(Offset.zero);
+    }
+  }
+
+  void closeMenu() {
+    _overlayEntry.remove();
+    _animationController.reverse();
+    isMenuOpen = !isMenuOpen;
+  }
+
+  void openMenu() {
+    findButton();
+    _animationController.forward();
+    _overlayEntry = _overlayEntryBuilder();
+    Overlay.of(context)?.insert(_overlayEntry);
+    isMenuOpen = !isMenuOpen;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topLeft,
+      key: _key,
+      width: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+            border: Border.all(color: Colors.grey, width: 1.3),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+          BoxShadow(
+              color: Colors.grey,
+              blurRadius: 4.0,
+              offset: Offset(0, 4.0)),
+        ]),
+
+      child: IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: _animationController,
+        ),
+        color: Colors.black,
+        onPressed: () {
+          if (isMenuOpen) {
+            closeMenu();
+          } else {
+            openMenu();
+          }
+        },
+      ),
+    );
+  }
+
+  OverlayEntry _overlayEntryBuilder() {
+    return OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          top: buttonPosition.dy + buttonSize.height,
+          left: buttonPosition.dx,
+          width: 150,
+          child: Material(
+            color: Colors.transparent,
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: ClipPath(
+                    clipper: ArrowClipper(),
+                    child: Container(
+                      width: 17,
+                      height: 17,
+                      color: widget.backgroundColor,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Container(
+                    width: widget.icons.length * buttonSize.width,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1.3),
+                      color: widget.backgroundColor,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [BoxShadow(color: Colors.grey,
+                          blurRadius: 4.0,
+                          offset: Offset(0, 4.0))
+                        ],
+                    ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(widget.icons.length, (index) {
+                          return GestureDetector(
+                            onTap: () {
+                              widget.onChange(index);
+                              closeMenu();
+                            },
+                            child: Row(children: [const Padding(padding: EdgeInsets.only(top: 50, left: 5)),
+                              Text((widget.icons[index] as Item).name, style: GoogleFonts.roboto(fontSize: 19),),
+                              const Padding(padding: EdgeInsets.only(right: 8)),
+                              (widget.icons[index] as Item).icon,
+                          ]),
+                          );
+                        }),
+                      ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/tasksList/TaskList.dart
+++ lib/tasksList/TaskList.dart
@@ -1,24 +1,81 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'TaskWidget.dart';
 import 'package:flutter/material.dart';
 import 'data/Task.dart';
 import 'data/TasksDao.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 
+import 'FilterDropDown.dart';
+
+class Item {
+  const Item(this.name,this.icon);
+  final String name;
+  final Container icon;
+}
+
+  List tasksDropDown = [
+    Item('All Tasks',Container()),
+    Item('Done',Container(
+        padding: const EdgeInsets.only(right: 15),
+        child:SvgPicture.asset('assets/images/done.svg',
+            height:22,
+            width: 22
+        ))),
+    Item('In Progress',Container(
+        padding: const EdgeInsets.only(right: 15),
+        child:SvgPicture.asset('assets/images/inProgress.svg',
+            height:22,
+            width: 22
+        ))),
+    Item('To Do',Container(
+        padding: const EdgeInsets.only(right: 15),
+        child:SvgPicture.asset('assets/images/toDo.svg',
+            height:22,
+            width: 22
+        ))),
+  ];
+
 class TaskListState extends State<TaskList> {
   final ScrollController _scrollController = ScrollController();
+  var currentIndex = 0;
+
+  filterState() {
+    switch (currentIndex) {
+      case 0:
+        return "all";
+      case 1:
+        return "done";
+      case 2:
+        return "inProgress";
+      case 3:
+        return "toDo";
+      default:
+        return null;
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Padding(
         padding: const EdgeInsets.only(left: 16.0, right: 16.0),
         child: Column(
-          children: [
-            const Padding(padding: EdgeInsets.only(top: 200)),
-            _getTaskList(),
-          ]
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 160)),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [FilterDropDown(
+                icons: tasksDropDown,
+                onChange: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),buildAddTask(context)]),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              _getTaskList(),
+            ]
         ),
       ),
     );
@@ -32,11 +89,50 @@ class TaskListState extends State<TaskList> {
         itemBuilder: (context, snapshot, animation, index) {
           final json = snapshot.value as Map<dynamic, dynamic>;
           final task = Task.fromJson(json);
-          return TaskWidget(task.title, task.state);
+          var state = filterState();
+          if (task.state == state || state == "all") {
+            return TaskWidget(task.title, task.state);
+          }
+          return Container();
         },
       ),
     );
   }
+}
+
+Widget buildAddTask(BuildContext context) {
+  var addTaskLogo = const Icon(Icons.add_box_outlined, size: 20, color: Color.fromRGBO(88, 84, 84, 1));
+
+  return TextButton(
+    style: ButtonStyle(
+        elevation: MaterialStateProperty.all<double>(4),
+        padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.only(left: 10, right: 10)),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+              side: const BorderSide(
+                  color: Colors.grey)
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.white)
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        addTaskLogo,
+        const Padding(padding: EdgeInsets.only(right: 5)),
+        Text("Add Task",
+          style: GoogleFonts.roboto(
+              fontSize: 17,
+              color: const Color.fromRGBO(88, 84, 84, 1)
+          ),
+        ),
+      ],
+    ),
+    onPressed: () {
+    },
+  );
 }
 
 class TaskList extends StatefulWidget {
